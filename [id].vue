553e38f8884ee8cd6b6805602352d9cc312dc341
booora26 <bgluvacevic@gmail.com>
2021-08-28 16:14:52 +0200

    Add files via upload

---
+++ [id].vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+ {{ $route.params.client }}
+  {{ $route.params.id }}
+    </div>
+</template>
+
+<script>
+    export default {
+       layout: 'custom' 
+        
+    }
+</script>
+
+<style lang="css" scoped>
+
+</style>
